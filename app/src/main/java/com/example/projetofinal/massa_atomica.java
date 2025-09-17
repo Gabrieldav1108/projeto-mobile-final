239833545861fcd397b71b3d7993dd1d8ee4061e
Gabriel Davila <gabrielsdavila2@gmail.com>
2025-09-16 21:47:47 -0300

    aaa

--- app/src/main/java/com/example/projetofinal/massa_atomica.java
+++ app/src/main/java/com/example/projetofinal/massa_atomica.java
@@ -2,17 +2,22 @@ package com.example.projetofinal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class massa_atomica extends AppCompatActivity {
+    ImageView btnMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +42,37 @@ public class massa_atomica extends AppCompatActivity {
             Intent i = new Intent(this, CalculadoraMassaAtomica.class);
             startActivity(i);
         });
+
+        btnMenu = findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+    }
+    private void showPopupMenu(View anchor) {
+        PopupMenu popup = new PopupMenu(this, anchor);
+        popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_massa_atomica) {
+                    Intent i = new Intent(anchor.getContext(), massa_atomica.class);
+                    anchor.getContext().startActivity(i);
+                    return true;
+                } else if (id == R.id.nav_outro) {
+                    Toast.makeText(anchor.getContext(), "Outro item clicado", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        popup.show();
     }
 }
